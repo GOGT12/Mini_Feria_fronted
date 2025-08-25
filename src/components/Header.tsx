ef18deae9f1201fa5f399f16a308a5b06df398a5
Gabriel Garnica <gabrielgarnica12@gmail.com>
2025-08-25 18:27:25 -0400

    mini feria fronted real fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -89,7 +89,7 @@ const Header = () => {
         </div>
 
         <div>
-          {isAdmin && (
+          {isAdmin || isSuperAdmin && (
             <div className="flex gap-2">
               <Link to={'/add-product/'}
               >
